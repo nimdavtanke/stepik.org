9b148e3d4efce78aeaf49f8a7d83aba918d48f6e
Zaytsev Denis <zayden@mail.ru>
2017-09-12 19:42:24 +0300

    work script

--- 1.12.6.py
+++ 1.12.6.py
@@ -4,6 +4,11 @@
 #
 ##################################
 #
-a=int(input('Input number:'))
-b=a+a
-print(b)
+n=int(input())
+
+if n%10 == 1 and n%100 != 11:
+    print(n,"программист")
+elif ((n%10 == 2) or (n%10 == 3) or (n%10 == 4)) and (n%100 != 12) and (n%100 != 13) and (n%100 != 14):
+    print(n,"программиста")
+else:
+    print(n,"программистов")

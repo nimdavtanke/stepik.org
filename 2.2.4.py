198449dc3956bab6f7ab6bb3bfcfef677a6d2bd8
Zaytsev Denis <zayden@mail.ru>
2017-09-13 16:49:08 +0300

    work script

--- 2.2.4.py
+++ 2.2.4.py
@@ -4,3 +4,15 @@
 #
 ##################################
 #
+
+count = 1
+
+while count > 0:
+    num = int(input())
+    count += 1
+    if num <= 10:
+        continue
+    elif num > 100:
+        break
+    else:
+        print(num)

3440325bb27f2c99f792b649428aa193a5882121
Zaytsev Denis <zayden@mail.ru>
2017-09-13 12:27:27 +0300

    work script

--- 1.12.7.py
+++ 1.12.7.py
@@ -4,3 +4,19 @@
 #
 ##################################
 #
+num=int(input())
+
+num6=num%10
+num5=(int(num/10))%10
+num4=(int(num/100))%10
+num3=(int(num/1000))%10
+num2=(int(num/10000))%10
+num1=int(num/100000)
+
+sum1=num1+num2+num3
+sum2=num4+num5+num6
+
+if sum1 == sum2 :
+    print("Счастливый")
+else:
+    print("Обычный")
